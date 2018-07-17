9cbe9595483b55af95de136c780ffafe90e953d1
sanchez.zou <sanchez.zou@hirede.com>
2018-07-17 16:01:12 +0800

    //lesson 5 //当主程序启动时定义了两个不同的线程。—个将被终止,另一个则会成功完成运行。 //在一个周期为30次迭代的区间中, 线程状态会从 ThreadState.Running变为ThreadState.WaitSleepJoin

--- Sanchez.zou.MultiThread/MultiThreading/Lesson_1/ThreadDemo.cs
+++ Sanchez.zou.MultiThread/MultiThreading/Lesson_1/ThreadDemo.cs
@@ -10,23 +10,45 @@ namespace MultiThreading.Lesson_1
 {
     public static class ThreadDemo
     {
+        private static string GetCurrentThreadId()
+        {
+            return $"current thread id:{CurrentThread.ManagedThreadId}";
+        }
+
         public static void PrintNumbers()
         {
             WriteLine("Starting ...");
             for (var i = 1; i < 10; i++)
             {
-                WriteLine($"Starting--{i}");
+                WriteLine($" Starting--{i}");
             }
         }
 
         public static void PrintNumbersWithDelay()
         {
-            WriteLine("Starting WithDelay ...");
+            WriteLine($"{GetCurrentThreadId()}--Starting WithDelay ...");
             for (var i = 1; i < 10; i++)
             {
                 Sleep(TimeSpan.FromSeconds(2));
-                WriteLine($"Starting WithDelay--{i}");
+                WriteLine($"{GetCurrentThreadId()}--Starting WithDelay--{i}");
             }
+        }
+
+        public static void PrintNumbersWithStatus()
+        {
+            WriteLine($"{GetCurrentThreadId()}--Starting WithStatus ...");
+            WriteLine($"当前线程状态：{CurrentThread.ThreadState}");
+            for (var i = 1; i < 10; i++)
+            {
+                Sleep(TimeSpan.FromSeconds(2));
+                WriteLine($"{GetCurrentThreadId()}--Starting WithStatus--{i}");
+            }
+        }
+
+        public static void DoNothing()
+        {
+            WriteLine($"{GetCurrentThreadId()}--Starting WithNothing ...");
+            Sleep(TimeSpan.FromSeconds(2));
         }
     }
 }
--- Sanchez.zou.MultiThread/MultiThreading/Program.cs
+++ Sanchez.zou.MultiThread/MultiThreading/Program.cs
@@ -17,7 +17,7 @@ namespace MultiThreading
             //lesson 1
             //结果两组范围为1到10的数字会随机交叉输出。这说明
             //PrintNumbers方法同时运行在主线程和另一个线程中。
-            //var t=new Thread(ThreadDemo.PrintNumbers);
+            //var t = new Thread(ThreadDemo.PrintNumbers);
             //t.Start();
             //ThreadDemo.PrintNumbers();
 
@@ -40,15 +40,37 @@ namespace MultiThreading
             //使用abort技术不一定总能终止线程。
             //目标线程可以通过处理该异常并调用Thread.ResetAbort方法来拒绝被终止。
             //因此并不推荐使用Abort方法来关闭线程。
-            WriteLine("Starting programing...");
-            var t4 = new Thread(ThreadDemo.PrintNumbersWithDelay);
-            t4.Start();
+            //WriteLine("Starting programing...");
+            //var t4 = new Thread(ThreadDemo.PrintNumbersWithDelay);
+            //t4.Start();
+            //Sleep(TimeSpan.FromSeconds(6));
+            //t4.Abort();
+            //WriteLine("A Thread has been aborted");
+            //var t41 = new Thread(ThreadDemo.PrintNumbers);
+            //t41.Start();
+            //ThreadDemo.PrintNumbers();
+
+            //lesson 5
+            //当主程序启动时定义了两个不同的线程。—个将被终止,另一个则会成功完成运行。
+            //在一个周期为30次迭代的区间中, 线程状态会从 ThreadState.Running变为ThreadState.WaitSleepJoin
+            //如果实际情况与以上不符,请增加迭代次数。
+            //终止第一个线程后,会看到现在该线程状态为Aborted。程序也有可能会打印出AbortRequested状态。
+            WriteLine("Starting program...");
+            var t5 = new Thread(ThreadDemo.PrintNumbersWithStatus);
+            var t51 = new Thread(ThreadDemo.DoNothing);
+            WriteLine(t5.ThreadState.ToString());
+            t51.Start();
+            t5.Start();
+            for (var i = 1; i < 30; i++)
+            {
+                WriteLine(t5.ThreadState.ToString());
+            }
+            //主线程被暂停，ThreadState状态改变
             Sleep(TimeSpan.FromSeconds(6));
-            t4.Abort();
-            WriteLine("A Thread has been aborted");
-            var t41 = new Thread(ThreadDemo.PrintNumbers);
-            t41.Start();
-            ThreadDemo.PrintNumbers();
+            t5.Abort();
+            WriteLine("a thread has been aborted ");
+            WriteLine(t5.ThreadState.ToString());
+            WriteLine(t51.ThreadState.ToString());
         }
     }
 }
