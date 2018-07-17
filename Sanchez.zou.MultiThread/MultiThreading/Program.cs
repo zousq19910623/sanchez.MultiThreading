6777996bf27b4e5b73551255fa5b37c4fab74570
sanchez.zou <sanchez.zou@hirede.com>
2018-07-17 14:45:21 +0800

    //lesson 1 //结果两组范围为1到10的数字会随机交叉输出。这说明 //PrintNumbers方法同时运行在主线程和另一个线程中。
    //lesson 2
    //如何暂停线程而不消耗操作系统资源

--- Sanchez.zou.MultiThread/MultiThreading/Program.cs
+++ Sanchez.zou.MultiThread/MultiThreading/Program.cs
@@ -13,19 +13,36 @@ namespace MultiThreading
     {
         static void Main(string[] args)
         {
+            //lesson 1
             //结果两组范围为1到10的数字会随机交叉输出。这说明
             //PrintNumbers方法同时运行在主线程和另一个线程中。
-            var t = new Thread(PrintNumbers);
-            t.Start();
+            //var t = new Thread(PrintNumbers);
+            //t.Start();
+            //PrintNumbers();
+
+            //lesson 2
+            //如何暂停线程而不消耗操作系统资源
+            var t2 = new Thread(PrintNumbersWithDelay);
+            t2.Start();
             PrintNumbers();
         }
 
-        static void PrintNumbers()
+        private static void PrintNumbers()
         {
             WriteLine("Starting ...");
             for (var i = 1; i < 10; i++)
             {
-                WriteLine(i);
+                WriteLine($"Starting--{i}");
+            }
+        }
+
+        private static void PrintNumbersWithDelay()
+        {
+            WriteLine("Starting WithDelay ...");
+            for (var i = 1; i < 10; i++)
+            {
+                Sleep(TimeSpan.FromSeconds(2));
+                WriteLine($"Starting WithDelay--{i}");
             }
         }
     }
