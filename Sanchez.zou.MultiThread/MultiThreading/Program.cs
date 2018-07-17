942f97585c08dfd7fddc8d8a84481141a5c1a112
sanchez.zou <sanchez.zou@hirede.com>
2018-07-17 15:13:52 +0800

    //lesson 4 //使用abort技术不一定总能终止线程。 //目标线程可以通过处理该异常并调用Thread.ResetAbort方法来拒绝被终止。 //因此并不推荐使用Abort方法来关闭线程。

--- Sanchez.zou.MultiThread/MultiThreading/Program.cs
+++ Sanchez.zou.MultiThread/MultiThreading/Program.cs
@@ -30,11 +30,25 @@ namespace MultiThreading
             //lesson 3
             //如何让程序等待另一个线程中的计算完成，使用Join
             //可以实现在两个线程间同步执行步骤
-            WriteLine("Starting ...");
-            var t3 = new Thread(ThreadDemo.PrintNumbersWithDelay);
-            t3.Start();
-            t3.Join();
-            WriteLine("Thread Completed");
+            //WriteLine("Starting ...");
+            //var t3 = new Thread(ThreadDemo.PrintNumbersWithDelay);
+            //t3.Start();
+            //t3.Join();
+            //WriteLine("Thread Completed");
+
+            //lesson 4
+            //使用abort技术不一定总能终止线程。
+            //目标线程可以通过处理该异常并调用Thread.ResetAbort方法来拒绝被终止。
+            //因此并不推荐使用Abort方法来关闭线程。
+            WriteLine("Starting programing...");
+            var t4 = new Thread(ThreadDemo.PrintNumbersWithDelay);
+            t4.Start();
+            Sleep(TimeSpan.FromSeconds(6));
+            t4.Abort();
+            WriteLine("A Thread has been aborted");
+            var t41 = new Thread(ThreadDemo.PrintNumbers);
+            t41.Start();
+            ThreadDemo.PrintNumbers();
         }
     }
 }
