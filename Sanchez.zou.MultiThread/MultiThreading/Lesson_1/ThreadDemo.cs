dd60ffb0683a386dfa9cf79485f14ceae7be4cea
sanchez.zou <sanchez.zou@hirede.com>
2018-07-17 14:59:10 +0800

    //lesson 3 //如何让程序等待另一个线程中的计算完成，使用Join //可以实现在两个线程间同步执行步骤

---
+++ Sanchez.zou.MultiThread/MultiThreading/Lesson_1/ThreadDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace MultiThreading.Lesson_1
+{
+    public static class ThreadDemo
+    {
+        public static void PrintNumbers()
+        {
+            WriteLine("Starting ...");
+            for (var i = 1; i < 10; i++)
+            {
+                WriteLine($"Starting--{i}");
+            }
+        }
+
+        public static void PrintNumbersWithDelay()
+        {
+            WriteLine("Starting WithDelay ...");
+            for (var i = 1; i < 10; i++)
+            {
+                Sleep(TimeSpan.FromSeconds(2));
+                WriteLine($"Starting WithDelay--{i}");
+            }
+        }
+    }
+}
--- Sanchez.zou.MultiThread/MultiThreading/Program.cs
+++ Sanchez.zou.MultiThread/MultiThreading/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MultiThreading.Lesson_1;
 using static System.Console;
 using static System.Threading.Thread;
 
@@ -16,34 +17,24 @@ namespace MultiThreading
             //lesson 1
             //结果两组范围为1到10的数字会随机交叉输出。这说明
             //PrintNumbers方法同时运行在主线程和另一个线程中。
-            //var t = new Thread(PrintNumbers);
+            //var t=new Thread(ThreadDemo.PrintNumbers);
             //t.Start();
-            //PrintNumbers();
+            //ThreadDemo.PrintNumbers();
 
             //lesson 2
-            //如何暂停线程而不消耗操作系统资源
-            var t2 = new Thread(PrintNumbersWithDelay);
-            t2.Start();
-            PrintNumbers();
-        }
+            //如何暂停线程而不消耗操作系统资源，使用Sleep
+            //var t2 = new Thread(ThreadDemo.PrintNumbersWithDelay);
+            //t2.Start();
+            //ThreadDemo.PrintNumbers();
 
-        private static void PrintNumbers()
-        {
+            //lesson 3
+            //如何让程序等待另一个线程中的计算完成，使用Join
+            //可以实现在两个线程间同步执行步骤
             WriteLine("Starting ...");
-            for (var i = 1; i < 10; i++)
-            {
-                WriteLine($"Starting--{i}");
-            }
-        }
-
-        private static void PrintNumbersWithDelay()
-        {
-            WriteLine("Starting WithDelay ...");
-            for (var i = 1; i < 10; i++)
-            {
-                Sleep(TimeSpan.FromSeconds(2));
-                WriteLine($"Starting WithDelay--{i}");
-            }
+            var t3 = new Thread(ThreadDemo.PrintNumbersWithDelay);
+            t3.Start();
+            t3.Join();
+            WriteLine("Thread Completed");
         }
     }
 }
